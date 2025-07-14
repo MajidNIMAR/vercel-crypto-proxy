22dc4764ab34d8fb30a2059a1aa0b8fb07c36e94
Majid <91701676+MajidNIMAR@users.noreply.github.com>
2025-07-14 02:01:56 +0200

    Mise à jour

--- api/btc.js
+++ api/btc.js
@@ -1,6 +1,3 @@
-export default async function handler(req, res) {
-  const response = await fetch('https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=30');
-  const data = await response.json();
-  res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate');
-  res.status(200).json(data);
+export default function handler(req, res) {
+  res.status(200).json({ coin: "Bitcoin", status: "OK" });
 }
